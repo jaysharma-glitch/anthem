694c4b1468ea1d512331347997fb9f215a570ed8
Jay <jayzzzzzsharma@gmail.com>
2020-10-08 19:02:31 +0530

    contact buton color changed. Chatbox typing visible

--- js/index.js
+++ js/index.js
@@ -2,21 +2,7 @@
 
 // Chatbox
 
-// $(".chat-input input").keyup(function (e) {
-//   if ($(this).val() == "") $(this).removeAttr("good");
-//   else $(this).attr("good", "");
-// });
-
-// function changeStyle(navWidth) {
-//   if (navWidth.matches) {
-//     $(".invisible-sm").css({ visibility: "hidden", position: "absolute" });
-//   } else {
-//     $(".invisible-sm").css({ visibility: "visible", position: "relative" });
-//   }
-// }
-
-// let navWidth = window.matchMedia("(max-width: 991px)");
-// changeStyle(navWidth);
-// //let width1 = width.__proto__;
-// //width1.adListener(changeStyle);
-// navWidth.adListener(changeStyle);
+$(".chat-input input").keyup(function (e) {
+  if ($(this).val() == "") $(this).removeAttr("good");
+  else $(this).attr("good", "");
+});
