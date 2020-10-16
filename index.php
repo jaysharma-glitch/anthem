05f19ad1de69457010166924d0c6af9e77e17e9e
Jay <jayzzzzzsharma@gmail.com>
2020-10-16 17:19:38 +0530

    rachaels project merged

--- index.php
+++ index.php
@@ -14,9 +14,56 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
+    echo "<script>
+         $(window).load(function(){
+             $('#thankyouModal').modal('show');
+         });
+    </script>";
+    // echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
     
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
     ?>
 
+
+
+<!-- Preferable use this - with sanitization -->
+
+
+
+<?php
+if(isset($_POST["submit"])) {
+        // Checking For Blank Fields..
+    if($_POST["full_name"]==""||$_POST["email"]==""||$_POST["number"]==""||$_POST["message"]==""){
+       echo "Please fill out everything as it helps us get in touch with you sooner";}
+    else
+        {
+        // Check if the "Sender's Email" input field is filled out
+        $email=$_POST['email'];
+                // Sanitize E-mail Address
+        $email =filter_var($email, FILTER_SANITIZE_EMAIL);
+                // Validate E-mail Address
+        $email= filter_var($email, FILTER_VALIDATE_EMAIL);
+        $emailConfirmed=$_POST['email'];
+        if (!$email){
+          echo "Don't forget to include your email address!";
+                }
+                else
+                {
+                    $subject = $_POST['number'];
+                    $message = $_POST['message'];
+                    $headers =  'From:' . $emailConfirmed . "\r\n"; // Sender's Email
+                    $headers .= 'Cc:' . $emailConfirmed . "\r\n"; // Carbon copy to Sender
+                    // Message lines should not exceed 70 characters (PHP rule), so wrap it
+                    $message = wordwrap($message, 70);
+                    // Send Mail By PHP Mail Function
+                    mail("jaysureshsharma@gmail.com", $subject, $message, $headers);
+                      echo "<script>
+         $(window).load(function(){
+             $('#thankyouModal').modal('show');
+         });
+    </script>";
+                };
+    }
+ }
+?>
--- js/animate.js
+++ js/animate.js
@@ -161,3 +161,4 @@ function handleClearButtonClick(event) {
 
 
 init();
+
